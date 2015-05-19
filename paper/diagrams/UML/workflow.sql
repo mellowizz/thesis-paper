c503534c62d4053a66b104d7a9cf58eb4378781b
Moran <niklasmoran@mailbox.tu-berlin.de>
2015-05-19 15:04:05 +0200

    first real commit.

---
+++ paper/diagrams/UML/workflow.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE tr_saarburg as
+SELECT *
+FROM tr_4511 as f, "public"."Saarburg_extent" as e
+WHERE ST_WITHIN(f.wkb_geometry, e.the_geom);
+
+CREATE INDEX tr_saarburg_idx ON tr_saarburg USING GIST ( wkb_geometry );
+
+alter table tr_saarburg
+drop the_geom;
